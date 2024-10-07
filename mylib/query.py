ee4e04bd9d8fa8a13cdf9e23a8df4ac5ec7e40ca
Jay <liuliangcheng@JaydeMacBook-Pro.local>
2024-10-06 23:10:03 -0400

    update

--- mylib/query.py
+++ mylib/query.py
@@ -15,18 +15,18 @@ def query():
 
     # Perform INSERT only if the record does not already exist
     cursor.execute(
-        "SELECT COUNT(*) FROM BirthDataDB WHERE year = 2000 AND month = 1"
+        "SELECT COUNT(*) FROM BirthDataDB WHERE year = 2000 AND month = 1 "
         "AND date_of_month = 15"
     )
     if cursor.fetchone()[0] == 0:
         cursor.execute(
             """
-        INSERT INTO BirthDataDB (
-            year, month, date_of_month, day_of_week, births
-        ) VALUES (
-            2000, 1, 15, 7, 9500
-        )
-        """
+            INSERT INTO BirthDataDB (
+                year, month, date_of_month, day_of_week, births
+            ) VALUES (
+                2000, 1, 15, 7, 9500
+            )
+            """
         )
 
     # Perform UPDATE
